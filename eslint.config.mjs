16b56cf22e66a01f59a5d8edc820839b2084781b
iaiakedmeyDev <magaiaiabubar@gmail.com>
2025-08-11 23:19:42 +0000

    configurações ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import json from "@eslint/json";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], 
+    plugins: { js },
+    extends: ["js/recommended"], 
+    rules: {
+			"no-unused-vars": "warn",
+			"no-undef": "warn",
+      "indent":[
+        "error",
+        4
+      ],
+      "linebreak-style":[
+        "error",
+        "windows"
+      ],
+      "quotes": [
+        "error",
+        "double"
+      ],
+      "semi": [
+        "error",
+        "always"
+      ]
+		},
+    languageOptions: { globals: globals.node } },
+
+  tseslint.configs.recommended,
+  { files: ["**/*.json"], 
+    plugins: { json }, 
+    language: "json/json", 
+    extends: ["json/recommended"] },
+  { files: ["**/*.jsonc"], 
+    plugins: { json }, 
+    language: "json/jsonc", 
+    extends: ["json/recommended"] },
+]);
